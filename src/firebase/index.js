21625c5cd9ce2f60e51e2bd0d892b87c9db5797e
V-Blaze <valentine6586@gmail.com>
2023-02-18 09:20:49 +0100

    Set up firebase configuration

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgEeJuUba_XNTsdHa0WNtpqgGWmFmALoo",
+    authDomain: "ico-launchpad-2b84a.firebaseapp.com",
+    projectId: "ico-launchpad-2b84a",
+    storageBucket: "ico-launchpad-2b84a.appspot.com",
+    messagingSenderId: "90438970595",
+    appId: "1:90438970595:web:5d4489d0980ff422bb29d6"
+  };
+  
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
