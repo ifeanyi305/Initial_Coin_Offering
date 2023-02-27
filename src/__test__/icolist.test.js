df0f0eb9f0dc6af62d1504714ae4d0fe86956372
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-02-27 21:39:44 +0100

    fix linter error

--- src/__test__/icolist.test.js
+++ src/__test__/icolist.test.js
@@ -1,13 +1,13 @@
-import Icolist from '../pages/IcoList'
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import Icolist from '../pages/IcoList';
 
-it("renders Icolist page", () => {
+it('renders Icolist page', () => {
   render(
-  <BrowserRouter>
-    <Icolist />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Icolist />
+    </BrowserRouter>,
   );
-  screen.getByRole('searchbox')
-  screen.getByRole('combobox')
-})
+  screen.getByRole('searchbox');
+  screen.getByRole('combobox');
+});
